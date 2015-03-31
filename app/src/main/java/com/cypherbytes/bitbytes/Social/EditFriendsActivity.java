a0f0c2026b86054e897c735cfb19c38906eb244c
travis <avey.travis@gmail.com>
2015-03-30 22:32:43 -0500

    Added log.v for errors

--- app/src/main/java/com/cypherbytes/bitbytes/Social/EditFriendsActivity.java
+++ app/src/main/java/com/cypherbytes/bitbytes/Social/EditFriendsActivity.java
@@ -3,19 +3,24 @@ package com.cypherbytes.bitbytes.Social;
 import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 
 import com.cypherbytes.bitbytes.DialogFragments.AlertDialogFragment;
 import com.cypherbytes.bitbytes.R;
 import com.cypherbytes.bitbytes.constants.ParseConstants;
 import com.parse.FindCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -27,6 +32,9 @@ public class EditFriendsActivity extends ListActivity
     @InjectView(R.id.friendsProgressBar) ProgressBar mProgressBar;
     protected List<ParseUser> mUsers;
 
+    protected ParseRelation<ParseUser> mFriendsRelation;
+    protected ParseUser mCurrentUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -35,6 +43,8 @@ public class EditFriendsActivity extends ListActivity
         ButterKnife.inject(this);
 
         mProgressBar.setVisibility(View.INVISIBLE);
+
+        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
     }
 
     @Override
@@ -42,7 +52,12 @@ public class EditFriendsActivity extends ListActivity
     {
         super.onResume();
 
+
+
         mProgressBar.setVisibility(View.VISIBLE);
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
 
         ParseQuery<ParseUser> query = ParseUser.getQuery();
         query.orderByAscending(ParseConstants.KEY_USERNAME);
@@ -66,8 +81,43 @@ public class EditFriendsActivity extends ListActivity
                     }
                     ArrayAdapter<String> adapter = new ArrayAdapter<String>(EditFriendsActivity.this, android.R.layout.simple_list_item_checked, usernames);
                     setListAdapter(adapter);
+
+                    addFriendCheckmarks();
                 } else {
                     // something went wrong
+                    Log.e("onResume", "at line 88", e);
+                    AlertDialogFragment dialog = new AlertDialogFragment();
+                    dialog.setMessage(e.getMessage());
+                    dialog.show(getFragmentManager(), "error_dialog");
+                }
+            }
+        });
+    }
+
+    private void addFriendCheckmarks()
+    {
+        mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>()
+        {
+            @Override
+            public void done(List<ParseUser> friends, ParseException e)
+            {
+                if (e == null)
+                {
+                    // list returned -- look for a match
+                    for (int i=0;i<mUsers.size();i++)
+                    {
+                        ParseUser user = mUsers.get(i);
+
+                        for(ParseUser friend : friends)
+                        {
+                            if(friend.getObjectId().equals(user.getObjectId()))
+                            {
+                                getListView().setItemChecked(i, true);
+                            }
+                        }
+                    }
+                } else {
+                    Log.e("addFriendCheckmarks", "at line 119", e);
                     AlertDialogFragment dialog = new AlertDialogFragment();
                     dialog.setMessage(e.getMessage());
                     dialog.show(getFragmentManager(), "error_dialog");
@@ -99,5 +149,35 @@ public class EditFriendsActivity extends ListActivity
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id)
+    {
+        super.onListItemClick(l, v, position, id);
+
+        if(getListView().isItemChecked(position))
+        {
+            // add friend
+            mFriendsRelation.add(mUsers.get(position));
+            mCurrentUser.saveInBackground(new SaveCallback()
+            {
+                @Override
+                public void done(ParseException e)
+                {
+                    if (e != null)
+                    {
+                        // error!
+                        Log.e("onItemListClick", "at line 169", e);
+                        AlertDialogFragment dialog = new AlertDialogFragment();
+                        dialog.setMessage(e.getMessage());
+                        dialog.show(getFragmentManager(), "error_dialog");
+                    }
+                }
+            });
+        } else {
+            // remove friend
+        }
+
     }
 }
